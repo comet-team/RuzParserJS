fa06c53eb26cd2e019236eb2380b932f64746bbf
Egor Molchan <90382636+egobars@users.noreply.github.com>
2022-09-18 13:38:48 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var request = require('request')
+var router = express.Router();
+
+async function getGroupId(group_name) {
+    let url = encodeURI('https://ruz.hse.ru/api/search?term=' + group_name + '&type=group');
+    let search = new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            resolve(JSON.parse(body));
+        });
+    });
+    let groups_list = await(search);
+    for (let i = 0; i < groups_list.length; ++i) {
+        if (groups_list[i].label === group_name) {
+            return groups_list[i].id;
+        }
+    }
+    return '0';
+}
+
+async function getWeekSchedule(group_id) {
+    let url = encodeURI('https://ruz.hse.ru/api/schedule/group/' + group_id + '?start=2022.09.19&finish=2022.09.25&lng=1');
+    let schedule = new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            resolve(JSON.parse(body));
+        });
+    });
+    return await (schedule);
+}
+
+async function sendData(schedule, chat_id) {
+    let url = 'http://74f8-217-150-192-102.ngrok.io/notify';
+    let data = {
+        "schedule": schedule,
+        "chat_id": chat_id
+    };
+    let send = new Promise((resolve, reject) => {
+        request.post(url, JSON.stringify(data), (error, response) => {
+            resolve(response.statusCode);
+        });
+    });
+    return await (send);
+}
+
+async function helper(group_name, chat_id) {
+    let group_id = await getGroupId(group_name);
+    let schedule = await getWeekSchedule(group_id);
+    let send = await sendData(schedule, chat_id);
+    console.log(send);
+}
+
+router.get('/', function(req, res, next) {
+    let group_name = req.query.group_name;
+    let chat_id = req.query.chat_id;
+    helper(group_name, chat_id).then((response) => {
+        res.status(200).send('');
+    });
+});
+
+module.exports = router;
